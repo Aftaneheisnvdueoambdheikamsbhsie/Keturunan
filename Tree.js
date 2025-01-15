ab30ef1863195b651f856ce8d98a88c730f65c65
aftacreative <aftaam6@gmail.com>
2025-01-15 16:21:24 +0700

    Update Tree.js

--- Tree.js
+++ Tree.js
@@ -1,52 +1,217 @@
-// Data keluarga Dakumi
+
 const familyData = {
-    name: "A. Dakumi",
+    name: "Dakumi",
     children: [
         {
             name: "Suryami",
             spouse: "Carsiyan",
             children: [
-                { name: "Sa'ad", spouse: "Suyem" },
-                { name: "Waryu", spouse: "Supari" },
-                { name: "Sibeng", spouse: "Wardan" },
-                { name: "Tabyan", spouse: "Saruni" },
-                { name: "Taiyah", spouse: "Casyono" },
+                {
+                    name: "Sa'ad",
+                    spouse: "Suyem",
+                    children: [
+                        { name: "Wartoyo", spouse: "Orang China", children: [] },
+                        { name: "Mundriyah", spouse: "Orang Palembang", children: [] },
+                        { name: "Kasmudi", spouse: "Turah", children: [] },
+                        { name: "Duriyah (Sidur)", spouse: "Orang Sumatera", children: [] },
+                        {
+                            name: "Yuti",
+                            spouse: "Yasin",
+                            children: [
+                                { name: "Fani Setiawan" },
+                                { name: "Faris" }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    name: "Waryu",
+                    spouse: "Supari",
+                    children: [
+                        { name: "Kartoyo", spouse: "Darsini", children: [] },
+                        { name: "Yudi", spouse: "Orang Bumirejo", children: [] },
+                        { name: "Katrun", spouse: "Orang Sembung", children: [] },
+                        { name: "Siwu (Wunipah)", spouse: "Sitol", children: [] },
+                        { name: "Taryi", spouse: "tidak diketahui", children: [] }
+                    ]
+                },
+                {
+                    name: "Sibeng",
+                    spouse: "Wardan",
+                    children: [
+                        {name:"Sri", spouse:"Ranya'", children:[{name:"Eli"},{name:"Tuginem"}]},
+                        {name:"Sun",spouse:"Tayain", children:[{name:"Riyanto"},{name:"Rohati"},{name:"Sigit"}]},
+                        {name: "Wagi", spouse: "Dirjo", children:[{name:"laki 1"},{name:"cewe 1"}]},
+                        
+                },
+                {
+                    name: "Tabyan",
+                    spouse: "Saruni",
+                    children: [
+                        {name:"Rumyati", spouse:"Bejo", children:[]},
+                        {name:"Duri",spouse:"Mustofiya", children:[]},
+                        {name: "Kunari", spouse: "Tosirin", children:[]},
+                        {name: "Kartono",spouse: "Orang pagergunung" , children:[]},
+                        {name:"Sikris(Krisyanto)",spouse:"(Putune Kastolani)",children:[]},
+                        {name:"Ningsih", spouse:"(gatau simbahe)", children[]},
+                        ]
+                },
+                {
+                    name: "Taiyah",
+                    spouse: "Casyono",
+                    children: [
+                        {name:"Di blendung gatau"}]
+                },
+                // Anak lainnya dari Suryami...
             ]
         },
         {
             name: "Ndari",
             spouse: "Kendho (Sutoyo)",
             children: [
-                { name: "Mukri", spouse: "Rahayu" },
-                { name: "Casmirah", spouse: "Kusen" },
-                { name: "Suryan", spouse: "Muryati" },
-                { name: "Rasiwen", spouse: "Radan" },
-                { name: "Comong", spouse: null },
+                {
+                    name: "Mukri",
+                    spouse: "Rahayu",
+                    children: [
+                        { name: "Belum diketahui (5 anak)" }
+                    ]
+                },
+                {
+                    name: "Casmiah",
+                    spouse: "Kusen",
+                    children: [{name:"Gapunya anak"}]
+                },
+                    {
+                    name: "Suryan",
+                    spouse: "Muryati",
+                    children: [
+                        {name:"Heri Mugiono", spouse:"Turip(anake h.Asroh", children:[]},
+                        {name:"Yitno",spouse:"Turiyah", children:[]},
+                        {name: "Heni", spouse: "(orang Sragi)", children:[]},
+                        {name: "Gathot Sumanto",spouse: "(anake Mus Sembung)" , children:[]},
+                        {name:"Sugeng",spouse:"Tidak diketahui(orang jauh)",children:[]},
+                     
+                    ]
+                },
+                 {
+                    name: "Rasiwen",
+                    spouse: "Radan",
+                    children: [
+                        {name:"Janji", spouse:"(pernah sama sinok)", children:[]},
+                        {name:"Tarman",spouse:"(orang ulujami)", children:[]},
+                        {name: "Sikun", spouse: "(orang Tuban)", children:[]},
+                        {name: "Wadiyo",spouse: "(Tidak diketahui)" , children:[]},
+                        {name:"Badi",spouse:"Tidak diketahui(anaknya polisi Suratman)",children:[]},
+                     
+                    ]
+                },
+                 {
+                    name: "Comong(Perawan Mati)",
+                    spouse: null,
+                    children: []
+                },
+                // Anak lainnya dari Ndari...
             ]
         },
         {
             name: "Siru",
             spouse: "Narsid",
             children: [
-                { name: "Sahuri", spouse: "Ilin", children: [{ name: "Isworo" }] },
-                { name: "Rasidah", spouse: "Sinang" },
-                { name: "Casmirah", spouse: "Sumarto" },
-                { name: "Kembur (Waryadi)", spouse: null },
+                {
+                    name: "Sahuri",
+                    spouse: "Ilin",
+                    children: [
+                        { name: "Isworo", spouse: "Bu Parni",children:[{name:"Liya"},{name:"Tri"}] }
+                        { name: "Tuti", spouse:"Suwarno",children:[{name: "belum diketahui"}]},
+                        { name: "Julaiha", spouse:"(Orang Cipete jakarta)",children:[{name:"belum diketahui"}]},
+                        { name: "Rejeh Biyatun", spouse:"(orang cipete juga)",children:[{name:"belum diketahui"}]},
+                    ]
+                },
+                {
+                    name: "Rasidah",
+                    spouse: "Sinang",
+                    children: []
+                },
+                     {
+                    name: "Casmirah",
+                    spouse: "Sumarto",
+                    children: []
+                },
+                 {
+                    name: "Kembur(Waryadi)",
+                    spouse: "Siru",
+                    children: [{name:"Gapunya anak"}]
+                },
+                // Anak lainnya dari Siru...
             ]
         },
         {
             name: "Redi Rahayu",
             spouse: "Yasir",
             children: [
-                { name: "Casiun", spouse: "Asari" },
-                { name: "Sunaryo", spouse: null },
-                { name: "Saryu (Gombol)", spouse: "Suhari" },
-                { name: "Sayumi", spouse: "Amat Nuryan" },
-                { name: "Sayi", spouse: "Nursaid" },
+                {
+                    name: "Casiun",
+                    spouse: "Asari",
+                    children: []
+                },
+                {
+                    name: "Sunaryo",
+                    spouse: "indun",
+                    children: []
+                },
+                {
+                    name: "Saryu",
+                    spouse: "Suhari",
+                    children: [
+                        {name:"sismanto", spouse:"manisah", children:[{name:"Huda"},{name:"Agus",{name:"Sakti"},{name:"Bunga"}]},
+                        {name:"kusriyati",spouse:"waryun", children:[{name:"Sandi"},{name:"Fahmi"}]},
+                        {name: "winarni", spouse: "eko", children:[{name:"Aliyah"},{name:"M. Davi"}, {name:""}]},
+                        {name: "heni",spouse: "Karyanto" , children:[{name:"Bima"},{nama:"Dini"},{name:"Noval"}]},
+                        {name:"nuryanto",spouse:"(Di jakarta)",children:[]},
+                     
+                },
+                {
+                    name: "Sayumi",
+                    spouse: "Amat Nuryan",
+                    children: [
+                        { name: "Rozikin", spouse: "Khusnul Khotimah", children: [{name:"Afta Aulia Mifda"},{name:"Aqila Azza Lia"},{name:"Alfaj dinul haq"}] },
+                        { name: "Sihu", spouse: "Hamidah", children: [{name:"Firman"},{name:"Gilang"},{name:"Baihaqi"}] },
+                        { name: "Nur Iman", spouse: "Eka", children: [{name:"Kia"},{name:"Hafidz"},{name:"Kanaya"}] },
+                        { name: "Sujai", spouse: "Otin Nuraeni", children: [{name:"Alisha Khoiro wilda"},{name:"Akifah husna ramadhani"},{name:"Anindya shaidah putri"}] },
+                        {
+                            name: "Munasiah",
+                            spouse: "Irwansyah",
+                            children: [
+                                { name: "Arsyila" },
+                              
+                            ]
+                        }
+                    ]
+                },
+                       
+                {
+                    name: "Mustofiyah",
+                    spouse: "Waluyo",
+                    children: [{name:"Mahira salma"},{name:"Andin cahaya amira"}]
+                },
+                {
+                    name: "Sayi",
+                    spouse: "Nursaid",
+                    children: [
+                        {name:"Tosirin", spouse:"Kunari", children:[{name:"Alda Alifa"},{name:""},{name:""}]},
+                        {name:"Wigiarti",spouse:"warno", children:[{name:""},{name:""}]},
+                        {name: "Junaedi", spouse: "bojone 2 variabel", children:[{name:""},{name:""}, {name:""}]},
+                        {name: "Rustiyo",spouse: "Thoriq" , children:[{name:"Aldi"},{nama:""}]},
+                        {name:"Winarsih",spouse:"(Jihad)",children:[{name:"jihan",{name:"laki-laki,belum ketahuan"}]},
+                     
+                },
+                // Anak lainnya dari Redi Rahayu...
             ]
         }
     ]
 };
+
+// Fungsi pembuatan pohon keluarga tetap sama seperti sebelumnya...
 
 // Fungsi membuat pohon keluarga
 function createTree(data, container) {
